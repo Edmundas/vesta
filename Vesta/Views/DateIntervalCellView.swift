78e2b252574aa4f985bd82efa61fb85cde1ab25d
Edmundas Matusevicius <edmundasm@gmail.com>
2021-09-03 16:47:41 +0300

    Updated date interval cell view.

--- Vesta/Views/DateIntervalCellView.swift
+++ Vesta/Views/DateIntervalCellView.swift
@@ -11,35 +11,36 @@ struct DateIntervalCellView: View {
     let dateInterval: DateInterval
     
     var body: some View {
-        VStack {
-            HStack {
+        HStack {
+            VStack(alignment: .leading) {
+                Label("Date", systemImage: "calendar")
                 Label("Start", systemImage: "hourglass.bottomhalf.fill")
-                    .font(.subheadline)
-                Spacer()
-                Text(formattedDate(date: dateInterval.start))
-                    .font(.headline)
-            }
-            HStack {
                 Label("End", systemImage: "hourglass.tophalf.fill")
-                    .font(.subheadline)
-                Spacer()
-                Text(formattedDate(date: dateInterval.end))
-                    .font(.headline)
-            }
-            HStack {
                 Label("Duration", systemImage: "clock")
-                    .font(.subheadline)
-                Spacer()
-                Text(formattedDuration(duration: dateInterval.duration))
-                    .font(.headline)
             }
+            .font(.subheadline)
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text(formattedDate(date: dateInterval.start))
+                Text(formattedTime(date: dateInterval.start))
+                Text(formattedTime(date: dateInterval.end))
+                Text(formattedDuration(duration: dateInterval.duration))
+            }
+            .font(.headline)
         }
+        .padding(.vertical)
     }
     
     private func formattedDate(date: Date) -> String {
         let dateFormatter = DateFormatter()
-        dateFormatter.dateStyle = .short
-        dateFormatter.timeStyle = .short
+        dateFormatter.dateStyle = .medium
+        
+        return dateFormatter.string(from: date)
+    }
+    
+    private func formattedTime(date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = .medium
         
         return dateFormatter.string(from: date)
     }
@@ -48,15 +49,14 @@ struct DateIntervalCellView: View {
         let s = Int(duration) % 60
         let m = Int(duration) / 60
         let h = Int(duration) / 360
-        let d = Int(duration) / 8640
         
-        return String(format:"%02d:%02d:%02d:%02d", d, h, m, s)
+        return String(format:"%02d:%02d:%02d", h, m, s)
     }
 }
 
 struct DateIntervalCellView_Previews: PreviewProvider {
     static var previews: some View {
         DateIntervalCellView(dateInterval: DateInterval(start: Date(), duration: 100))
-            .previewLayout(.fixed(width: 400, height: 60))
+            .previewLayout(.sizeThatFits)
     }
 }
