e58c8b9b5118d01a198f945cc9c6d6c7f7f62df5
Edmundas Matusevicius <edmundasm@gmail.com>
2021-09-08 09:42:12 +0300

    Fixed bug in duration formatting.

--- Vesta/Helpers/DataFormatter.swift
+++ Vesta/Helpers/DataFormatter.swift
@@ -25,7 +25,7 @@ struct DataFormatter {
     static func formattedDuration(duration: Double) -> String {
         let s = Int(duration) % 60
         let m = Int(duration) / 60
-        let h = Int(duration) / 360
+        let h = Int(duration) / 3600
         
         return String(format:"%02d:%02d:%02d", h, m, s)
     }
