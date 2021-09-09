729885ef7aef9f2d3462c3f857c317f96ce6e0dc
Edmundas Matusevicius <edmundasm@gmail.com>
2021-09-09 14:55:18 +0300

    Updated presistence data for preview generation.

--- Vesta/Model/Persistence.swift
+++ Vesta/Model/Persistence.swift
@@ -13,10 +13,11 @@ struct PersistenceController {
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<10 {
+        for index in 0..<10 {
             let newTask = Task(context: viewContext)
             newTask.id = UUID()
-            newTask.title = "First task"
+            newTask.title = "Task #\(index + 1)"
+            newTask.userOrder = Int16(index + 1)
         }
         do {
             try viewContext.save()
